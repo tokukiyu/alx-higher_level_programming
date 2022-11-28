d6171f71e9f6c0738f94953af38608d073e0fc1a
Bekalu Geta <bekalu2054@gmail.com>
2022-11-28 19:07:20 +0300

    fix error

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,22 @@
 #include "lists.h"
-
-int check_cycle(listint *list)
+/**
+ * check_cycle - checks if the linked list is cyclic or not
+ * @list: pointer to the head of the list.
+ *
+ * Return: (1) if it is cyclic, otherwise (0).
+ */
+int check_cycle(listint_t *list)
 {
-listint *node = list->next;
-while (node != NULL && node != list)
-node = node->next;
-if (node == NULL)
-return (0);
-elseif (node == list)
-return (1);
+	listint_t *node = list->next;
+
+	if (list && list->next)
+	{
+		while (node != NULL && node != list)
+			node = node->next;
+		if (node == NULL)
+			return (0);
+		else if (node == list)
+			return (1);
+	}
+	return (0);
 }
