30220dc2ddb9cbaaa4d380e1196aa6ad9078e17c
Bekalu Geta <bekalu2054@gmail.com>
2022-12-01 21:39:21 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import sys
+
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
         exit()
+    num = len(sys.argv)
     if len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(num - 1))
 
     i = 0
     for word in sys.argv:
