32836b61ebf65f367d3c66605fc395f54773fd4a
Bekalu Geta <bekalu2054@gmail.com>
2022-12-01 20:42:23 +0300

    task 2

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: firdt integer
+    b: second integer
+
+    Returns:
+    The value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My substraction function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a *b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My divistion function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Return:
+    The return value. a / b
+    """
+    return (a / b)
