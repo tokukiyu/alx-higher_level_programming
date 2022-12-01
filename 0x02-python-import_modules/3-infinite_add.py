ee095297256358251586bba01d9523dbf194b588
Bekalu Geta <bekalu2054@gmail.com>
2022-12-01 22:15:26 +0300

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("0")
-        exit()
-    it, result = 0, 0
-    for numbers in sys.argv:
-        if it == 1:
-            continue
+    result = 0
+    for numbers in sys.argv[1:]:
         result += int(numbers)
     print(result)
