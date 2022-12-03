21ea99e995c3ee1a9f13d09bdbf1c839835bb24b
Bekalu Geta <bekalu2054@gmail.com>
2022-12-03 13:56:52 +0300

    yeah

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        idx = my
+    no_c_string = []
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            no_c_string.append(i)
+    return ''.join(no_c_string)
