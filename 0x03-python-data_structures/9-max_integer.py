0d9c6e72dd715b5596217d97540bd21946b096d9
Bekalu Geta <bekalu2054@gmail.com>
2022-12-03 16:19:14 +0300

    cool

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max_val = my_list[0]
+        for i in my_list:
+            if max_val < i:
+                max_val = i
+        return max_val
+    return None
