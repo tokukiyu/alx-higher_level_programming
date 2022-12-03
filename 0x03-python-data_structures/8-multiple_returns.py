8dc443555476be92eb5831a8ad6f10ffa0d87df7
Bekalu Geta <bekalu2054@gmail.com>
2022-12-03 16:11:32 +0300

    cool

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) <= 0:
-        a[0] = None
-        length = 0
-    length = len(sentence)
-    return (length, sentence[0])
+    if len(sentence) > 0:
+        return (len(sentence), sentence[0])
+    return (0, None)
