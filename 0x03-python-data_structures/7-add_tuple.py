5744a8e5dc334046b28a49c421ab62a3a97f2626
Bekalu Geta <bekalu2054@gmail.com>
2022-12-03 15:45:28 +0300

    nope

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,19 +4,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     try:
         a1 = tuple_a[0]
-    except:
+    except IndexError:
         a1 = 0
     try:
         a2 = tuple_a[1]
-    except:
+    except IndexError:
         a2 = 0
     try:
         b1 = tuple_b[0]
-    except:
+    except IndexError:
         b1 = 0
     try:
         b2 = tuple_b[1]
-    except:
+    except IndexError:
         b2 = 0
 
     result = (a1 + b1, a2 + b2)
