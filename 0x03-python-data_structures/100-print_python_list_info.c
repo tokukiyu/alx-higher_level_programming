26d52674fa3bd7e6a798223a05afe0d6301a9526
Bekalu Geta <bekalu2054@gmail.com>
2022-12-05 19:28:48 +0300

    fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include "python.h"
+#include <python.h>
 
 /**
  * print_python_list_info - prints some basic info about a python list object
