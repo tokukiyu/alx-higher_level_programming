51a990c53d071d5daf6c07b9dde4c4e9e60f9ec4
Bekalu Geta <bekalu2054@gmail.com>
2022-12-07 19:19:22 +0300

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    a_dictionary = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    a_dictionary = {'M': 1000,
+                    'D': 500,
+                    'C': 100,
+                    'L': 50,
+                    'X': 10,
+                    'V': 5,
+                    'I': 1}
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     prev, summ = 0, 0
 
     for x in roman_string:
-        summ += a_dictionary[x] if a_dictionary[x] <= prev else a_dictionary[x] - prev * 2
+        summ += a_dictionary[x] if a_dictionary[x] <= prev \
+                else a_dictionary[x] - prev * 2
         prev = a_dictionary[x]
     return summ
