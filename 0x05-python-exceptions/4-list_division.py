d248238f841df8ecf4afbb987e6150796abfa8c1
Bekalu Geta <bekalu2054@gmail.com>
2022-12-19 21:59:53 +0300

    task 5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,9 +2,11 @@
 def list_division(my_list_1, my_list_2, list_length):
 
     div_list = []
-    div = 0
 
     for i in range(list_length):
+
+        div = 0
+
         try:
             div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
