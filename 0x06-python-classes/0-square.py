cd670a6e34adf73ac8b51d0967e4ef5c695ee2ec
Bekalu Geta <bekalu2054@gmail.com>
2022-12-20 22:24:29 +0300

    fix pycodestyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/pyhton3
 
-""" a module that contain Square class"""
+"""a module that contain Square class"""
 
 
 class Square:
-
     """an empty class ``Square`` that defines a square"""
-
     pass
