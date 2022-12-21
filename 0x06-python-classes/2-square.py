f08cdf6678084f4d2f5953652a6a17772e87de5e
Bekalu Geta <bekalu2054@gmail.com>
2022-12-21 16:16:01 +0300

    fix

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""A Square class"""
+
+
+class Square:
+
+    """A simple Square with a size"""
+
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
