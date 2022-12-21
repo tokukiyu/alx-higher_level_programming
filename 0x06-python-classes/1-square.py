0295d22dd0865efdabebe19e0cc23bb121c62cff
Bekalu Geta <bekalu2054@gmail.com>
2022-12-21 16:06:14 +0300

    fix

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""this modul contains Square class"""
+
+
+class Square:
+    """a class Square that defines a square with private instance attribute"""
+    def __init__(self, value):
+        self.__size = value
