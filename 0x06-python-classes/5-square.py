10736fe769ccb2bc88fbc76df992838601718981
Bekalu Geta <bekalu2054@gmail.com>
2022-12-21 18:40:55 +0300

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" a square class"""
+
+
+class Square:
+    """a class"""
+
+    def __init__(self, size=0):
+        """intializer"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """size retriver"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """return area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ print #"""
+        if self.__size == 0:
+            print()
+        elif self.__size == 1:
+            print('#')
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#')
+                    if j == (self.__size - 1):
+                        print()
