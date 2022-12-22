6daf3019677cedbe91e68609e7715fe83aa7f468
Bekalu Geta <79188583+bekalue@users.noreply.github.com>
2022-12-22 11:21:58 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,22 +1,41 @@
 #!/usr/bin/python3
-""" a square class"""
+""" a module that contain square class
+"""
 
 
 class Square:
-    """a class"""
+    """Representation of 2D polygon with 4 equal and perpendicular sides
+    
+    Args:
+        size (int): attribute intializer parameter
+    Attributes:
+        __size (int): a private instance attribute.
+    """
 
     def __init__(self, size=0):
-        """intializer"""
+        """intializer
+        """
         self.__size = size
 
     @property
     def size(self):
-        """size retriver"""
+        """size retriver
+        
+        Returns:
+             int: size of the square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """size setter"""
+        """size setter(updates square size)
+        
+        Args:
+            Value (int): updates the size of square
+        Raises:
+            TypeError: if value is not integer type.
+            ValueError: if value is less than zero.
+        """
         if type(value) != int:
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -25,11 +44,16 @@ class Square:
             self.__size = value
 
     def area(self):
-        """return area"""
+        """computes area
+        
+        Returns:
+              int: area of the square
+        """
         return (self.__size ** 2)
 
     def my_print(self):
-        """ print #"""
+        """Prints a 2D table of the '#' symbol with the size of this square.
+        """
         if self.__size > 0:
             for i in range(self.__size):
                 for j in range(self.__size):
