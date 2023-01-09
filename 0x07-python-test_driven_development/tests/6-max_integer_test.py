c7c34d81acc7693200034545fd1dc4962d51a46d
Bekalu Geta <bekalu2054@gmail.com>
2023-01-09 20:20:15 +0300

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -19,7 +19,13 @@ class Test_max_integer(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1, 3, 43, -97, 50, 13]), 50)
         self.assertEqual(max_integer([5, 12, -12, 12, -12]), 12)
-        self.assertEqual(max_integer([2]), 4)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([7, 2, -3, 45, 6]), 45)
+        self.assertEqual(max_integer([7, 2, 45, 6]), 45)
+        self.assertEqual(max_integer([-7, -2, -45, -6]), -2)
+        self.assertEqual(max_integer([-7, 1, -18, -6]), 1)
+        self.assertEqual(max_integer([7, 1, -18, -6]), 7)
+        self.assertEqual(max_integer([-7, 1, -18, 9]), 9)
 
 if __name__ == "__main__":
     unittest.main()
