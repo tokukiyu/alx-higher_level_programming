b414d438ff3ff3df4cdeda6a8c43c9b7da298e51
Bekalu Geta <bekalu2054@gmail.com>
2023-01-09 16:52:34 +0300

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-A module containing a list subclass.
-"""
+'''A module containing a list subclass.
+'''
 
 
 class MyList(list):
-    """
-    A list subclass.
-    """
-
+    '''A list subclass.
+    '''
     def print_sorted(self):
-        """
-        Prints this list in a sorted order.
-        """
-        sortted_list = sorted(self)
-        print(sortted_list)
-        del sortted_list
+        '''Prints this list in a sorted order.
+        '''
+        list_sorted = sorted(self)
+        print(list_sorted)
+        del list_sorted
