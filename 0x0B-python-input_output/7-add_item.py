fc8e5b72270adb5206bab031b5a494944dea9c62
Bekalu Geta <bekalu2054@gmail.com>
2023-01-11 20:21:44 +0300

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,36 +3,15 @@
 usage: ./7-add_item.py ......... ....... ..
 '''
 import sys
-import os
-from importlib import import_module as using
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-save_to_json_file, load_from_json_file = (
-    using('5-save_to_json_file').save_to_json_file,
-    using('6-load_from_json_file').load_from_json_file
-)
-'''The functions for saving and loading lists.
-'''
-args_list = []
-'''The list of arguments.
-'''
-args_list_file_name = 'add_item.json'
-'''The file name of the file containing the list of arguments.
-'''
-
-
-def run():
-    '''Runs the routines of this script.
-    '''
-    if not os.path.exists(args_list_file_name):
-        with open(args_list_file_name, mode='w', encoding='utf-8') as file:
-            file.write('[]')
-    json_list = load_from_json_file(args_list_file_name)
-    if (type(json_list) is list) and all(type(s) is str for s in json_list):
-        args_list.extend(json_list)
-    args_list.extend(sys.argv[1:])
-    save_to_json_file(args_list, args_list_file_name)
-
-
-if __name__ == '__main__':
-    run()
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
