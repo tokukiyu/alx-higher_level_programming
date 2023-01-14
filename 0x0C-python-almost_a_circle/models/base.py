3bf79a2bf49bb1c3052c9cf593f954c78223ba47
Bekalu Geta <bekalu2054@gmail.com>
2023-01-14 16:31:49 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,34 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        Saves a list of polygons to a file in JSON format.
+
+        Args:
+            list_objs (list): A list of polygons.
+        '''
+
+        filename = '{}.json'.format(cls.__name__)
+        dict_list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                if type(obj) is cls:
+                    dict_list.append(obj.to_dictionary())
+        with open(filename, mode='w', encoding='utf-8') as f:
+            json.dump(dict_list, f)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''
+        Creates a list from its JSON representation.
+        Args:
+            json_string (str): A JSON string representation of a list.
+        Returns:
+            list: A JSON representation of the list of dictionaries.
+        '''
+        if json_string is None or len(json_string.strip()) == 0:
+            return []
+        return json.loads(json_string)
