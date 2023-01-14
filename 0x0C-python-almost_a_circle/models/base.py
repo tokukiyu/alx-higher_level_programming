9212705402d60e016beeacb2921910fce3d9a80d
Bekalu Geta <bekalu2054@gmail.com>
2023-01-14 16:13:56 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
+'''Contains classes for working with Polygons.
+'''
+
+
+import json
+
 
 class Base:
+    '''
+    Represents the base class for all polygon objects.
+    '''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        '''
+        Initializes a new polygon object with the given id.
+        Args:
+            id (int): The id of this polygon object.
+        '''
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+        Creates the JSON representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        Returns:
+            str: A JSON representation of the list of dictionaries.
+        '''
+
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
