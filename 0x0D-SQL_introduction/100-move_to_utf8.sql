0f31bdd47efb5c84fc0ba3d5e54d7a7fdadefe15
Bekalu Geta <bekalu2054@gmail.com>
2023-02-17 00:06:24 +0300

    change to utf

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,3 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-UPDATE first_table CONVERT name TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
