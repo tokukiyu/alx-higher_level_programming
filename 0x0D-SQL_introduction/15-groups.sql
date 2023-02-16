1af95ad769cdc0909b590aa28b685fdad536ac94
Bekalu Geta <bekalu2054@gmail.com>
2023-02-16 23:33:08 +0300

    count score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score
-SELECT score , count(score) AS number FROM second_table GROUP BY score WHERE score ASC;
+SELECT score , count(score) AS number FROM second_table GROUP BY score WHERE number DESC;
